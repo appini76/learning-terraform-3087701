00b977b48b956ddf296bc0c34efdc37d3f36b1ad
appini76 <46800859+appini76@users.noreply.github.com>
2024-07-28 08:27:46 -0700

    fixed errors

--- main.tf
+++ main.tf
@@ -41,19 +41,19 @@ resource "aws_security_group_rule" "blog_http_in" {
   from_port     = 80
   to_port       = 80
   protocol      = "http"
-  cidr_block    =  ["0.0.0.0/0"]
+  cidr_blocks    =  ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 } 
 
 resource "aws_security_group_rule" "blog_https_in" {
   type    = "ingress"
   from_port     = 443
   to_port       = 443
-  protocol      = "http"
-  cidr_block    =  ["0.0.0.0/0"]
+  protocol      = "https"
+  cidr_blocks    =  ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 } 
 
 resource "aws_security_group_rule" "blog_everything_out" {
@@ -61,7 +61,7 @@ resource "aws_security_group_rule" "blog_everything_out" {
   from_port     = 0
   to_port       = 0
   protocol      = "http"
-  cidr_block    =  ["0.0.0.0/0"]
+  cidr_blocks    =  ["0.0.0.0/0"]
 
-  aws_security_group_id = aws_security_group.blog.id
+  security_group_id = aws_security_group.blog.id
 } 
