89d6a0436fefbd7c230ce4e31d70cb645ecabf08
appini76 <46800859+appini76@users.noreply.github.com>
2024-07-28 10:00:40 -0700

    create new dev vpc

--- main.tf
+++ main.tf
@@ -20,6 +20,9 @@ resource "aws_instance" "blog" {
 
   vpc_security_group_ids = [module.blog_sg.security_group_id]
 
+  subnet_id     = module.blog_vpc.public_subnets[0]
+
+
   tags = {
     Name = "HelloWorld"
   }
@@ -29,13 +32,33 @@ data "aws_vpc" "default"{
   default = true
 }
 
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-1b", "us-west-1c"]
+
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
+
 module "blog_sg" {
   source   = "terraform-aws-modules/security-group/aws"
   version  = "4.13.0"
 
   name     = "blog"
 
-  vpc_id  = data.aws_vpc.default.id
+  //vpc_id  = data.aws_vpc.default.id
+  vpc_id    = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
