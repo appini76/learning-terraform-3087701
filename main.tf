48623fc7ebeeb83dbf297dd24f9a480fa585d62b
appini76 <46800859+appini76@users.noreply.github.com>
2024-07-28 10:32:48 -0700

    LB Fixed

--- main.tf
+++ main.tf
@@ -32,27 +32,20 @@ module "alb" {
   source = "terraform-aws-modules/alb/aws"
 
   name    = "blog-alb"
+
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups     = module.blog_sg.security_group_id
+  security_groups     = [module.blog_sg.security_group_id]
 
- 
-  target_groups = [
-    {
+  target_groups = {
+    ex-instance = {
       name_prefix      = "blog-"
-      backend_protocol = "HTTP"
-      backend_port     = 80
+      protocol         = "HTTP"
+      port             = 80
       target_type      = "instance"
+      target_id        = aws_instance.blog.id
     }
-  ]
-
-    http_tcp_listeners = [
-    {
-      port               = 80
-      protocol           = "HTTP"
-      target_group_index = 0
-    }
-  ]
+  }
 
   tags = {
     Environment = "dev"
